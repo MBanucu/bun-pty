7a9744dce9bb96dcb60810992a4ee61781606ce9
MBanucu <michael.banucu@googlemail.com>
2026-02-08 18:55:24 +0100

    build: add cross-compilation dev shells for Windows
    - Add zig and rustup to default devShell for cross-compilation support
    - Add new devShell.crossWindows for Windows cross-compilation
    - Include necessary tools: rustup, bun, bashInteractive

--- flake.nix
+++ flake.nix
@@ -14,12 +14,23 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        crossPkgs = nixpkgs.legacyPackages.${system}.pkgsCross.mingwW64;
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             cargo
             rustc
+            rustup  # Added for managing Rust targets
+            zig  # For zigbuild cross-compilation
+            bun
+            bashInteractive
+          ];
+        };
+
+        devShells.crossWindows = crossPkgs.mkShell {
+          buildInputs = with crossPkgs; [
+            rustup
             bun
             bashInteractive
           ];
