75bd0f2628565f177cf4db7c6f3476a7fa5d8bb5
MBanucu <michael.banucu@googlemail.com>
2026-02-08 20:50:21 +0100

    refactor: make common module public and update imports

--- rust-pty/src/platform/windows/pty_impl.rs
+++ rust-pty/src/platform/windows/pty_impl.rs
@@ -1,3 +1,2 @@
 // rust-pty/src/platform/windows/pty_impl.rs
-use super::super::common::PtyImpl as CommonPtyImpl; // reuse the common struct/impl
-pub use CommonPtyImpl; // expose the same name the rest of the crate expects
+pub use crate::platform::common::PtyImpl;
