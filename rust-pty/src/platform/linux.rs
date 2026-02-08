bb45db779a990e65f6337ba2341281a48f4fb39c
MBanucu <michael.banucu@googlemail.com>
2026-02-08 16:36:49 +0100

    perf: implement event-driven PTY architecture using control pipes for write, resize, and kill operations

--- rust-pty/src/platform/linux.rs
+++ rust-pty/src/platform/linux.rs
@@ -1,11 +1,10 @@
 use crate::pty::{Msg, PtyTrait, Reader};
-use crossbeam::channel::{unbounded, Sender};
+use crossbeam::channel::{unbounded};
 use portable_pty::{native_pty_system, PtySize, ChildKiller, MasterPty};
 use std::{
     sync::{Arc, Mutex, atomic::{AtomicBool, AtomicI32, Ordering}},
     thread,
-    os::unix::io::RawFd,
-    any::Any,
+    os::unix::io::{RawFd, AsRawFd},
     fs::File,
     io::Read,
 };
@@ -19,7 +18,6 @@ fn debug(msg: &str) {
 
 pub struct PtyImpl {
     reader: crate::pty::Reader,
-    tx_w: Sender<(Vec<u8>, usize)>,
     master: Arc<Mutex<Box<dyn MasterPty + Send>>>,
     killer: Arc<Mutex<Box<dyn ChildKiller + Send + Sync>>>,
     exited: AtomicBool,
@@ -35,11 +33,11 @@ impl PtyImpl {
         let pair = sys.openpty(size)?;
         let mut child = pair.slave.spawn_command(cmd.to_builder())?;
         let killer = Arc::new(Mutex::new(child.clone_killer()));
+        let killer_clone = killer.clone();
         let pid = child.process_id().map(|p| p as i32).unwrap_or(-1);
 
         /* channels */
         let (tx_r, rx_r) = unbounded::<Msg>();
-        let (tx_w, rx_w) = unbounded::<(Vec<u8>, usize)>();
 
         let master = Arc::new(Mutex::new(pair.master));
 
@@ -59,7 +57,6 @@ impl PtyImpl {
 
         let pty = Arc::new(Self {
             reader: Reader::new(rx_r),
-            tx_w,
             master: master.clone(),
             killer,
             exited: AtomicBool::new(false),
@@ -67,8 +64,6 @@ impl PtyImpl {
             pid,
             control_pipe,
         });
-
-        /* wait-thread */
         {
             let pty_clone = pty.clone();
             thread::spawn(move || {
@@ -90,36 +85,19 @@ impl PtyImpl {
             let mut rdr = master.lock().unwrap().try_clone_reader()?;
             let tx = tx_r.clone();
             let control_read_fd = pty.control_pipe[0];
+            let master_clone = master.clone();
+            let killer_clone = killer_clone.clone();
             thread::spawn(move || {
                 debug("read-thread started");
                 let mut buf = vec![0; 8192];
 
-                // Get PTY file descriptor by downcasting the reader
-                let pty_fd = if let Some(file) = (&*rdr as &dyn Any).downcast_ref::<File>() {
-                    file.as_raw_fd()
-                } else {
-                    debug("read-thread: failed to downcast reader to File, falling back to blocking read");
-                    // Fallback to old behavior if downcast fails
-                    loop {
-                        debug("read-thread: attempting read...");
-                        match rdr.read(&mut buf) {
-                            Ok(0) => {
-                                debug("read-thread: got Ok(0) - EOF");
-                                break;
-                            }
-                            Ok(n) => {
-                                debug(&format!("read-thread: got Ok({}) bytes", n));
-                                let _ = tx.send(Msg::Data(buf[..n].to_vec()));
-                            }
-                            Err(e) => {
-                                debug(&format!("read-thread: got Err: {}", e));
-                                break;
-                            }
-                        }
+                // Get PTY file descriptor by unsafe casting a temporary reader
+                let pty_fd = {
+                    let temp_rdr = master_clone.lock().unwrap().try_clone_reader().unwrap();
+                    unsafe {
+                        let file_ptr = (&*temp_rdr) as *const dyn Read as *const File;
+                        (*file_ptr).as_raw_fd()
                     }
-                    let _ = tx.send(Msg::End);
-                    debug("read-thread: ended");
-                    return;
                 };
 
                 debug(&format!("read-thread: got PTY fd {}, control fd {}", pty_fd, control_read_fd));
@@ -170,21 +148,69 @@ impl PtyImpl {
                     // Check for control pipe data (control events)
                     if pollfds[1].revents & libc::POLLIN != 0 {
                         debug("read-thread: control pipe has data");
-                        let mut control_buf = [0u8; 1];
-                        match unsafe { libc::read(control_read_fd, control_buf.as_mut_ptr() as *mut libc::c_void, 1) } {
-                            -1 => {
-                                debug(&format!("read-thread: control pipe read error: {}", std::io::Error::last_os_error()));
+                        
+                        // Read message type
+                        let mut msg_type_buf = [0u8; 1];
+                        if unsafe { libc::read(control_read_fd, msg_type_buf.as_mut_ptr() as *mut libc::c_void, 1) } != 1 {
+                            debug("read-thread: failed to read message type");
+                            continue;
+                        }
+                        
+                        let msg_type = msg_type_buf[0];
+                        debug(&format!("read-thread: received control message type: {}", msg_type));
+                        
+                        match msg_type {
+                            1 => { // Write
+                                // Read data length
+                                let mut len_buf = [0u8; 4];
+                                if unsafe { libc::read(control_read_fd, len_buf.as_mut_ptr() as *mut libc::c_void, 4) } != 4 {
+                                    debug("read-thread: failed to read write length");
+                                    continue;
+                                }
+                                let data_len = u32::from_le_bytes(len_buf) as usize;
+                                
+                                // Read data
+                                let mut data_buf = vec![0u8; data_len];
+                                if unsafe { libc::read(control_read_fd, data_buf.as_mut_ptr() as *mut libc::c_void, data_len) } != data_len as isize {
+                                    debug("read-thread: failed to read write data");
+                                    continue;
+                                }
+                                
+                                debug(&format!("read-thread: writing {} bytes", data_len));
+                                // Perform the write operation
+                                if let Err(e) = master_clone.lock().unwrap().take_writer().and_then(|mut wtr| {
+                                    wtr.write_all(&data_buf)?;
+                                    wtr.flush()?;
+                                    Ok(())
+                                }) {
+                                    debug(&format!("read-thread: write error: {}", e));
+                                }
                             }
-                            0 => {
-                                debug("read-thread: control pipe EOF");
+                            2 => { // Resize
+                                // Read rows and cols
+                                let mut size_buf = [0u8; 4];
+                                if unsafe { libc::read(control_read_fd, size_buf.as_mut_ptr() as *mut libc::c_void, 4) } != 4 {
+                                    debug("read-thread: failed to read resize data");
+                                    continue;
+                                }
+                                let rows = u16::from_le_bytes([size_buf[0], size_buf[1]]);
+                                let cols = u16::from_le_bytes([size_buf[2], size_buf[3]]);
+                                
+                                debug(&format!("read-thread: resizing to {}x{}", rows, cols));
+                                // Perform the resize operation
+                                if let Err(e) = master_clone.lock().unwrap().resize(PtySize { rows, cols, pixel_width: 0, pixel_height: 0 }) {
+                                    debug(&format!("read-thread: resize error: {}", e));
+                                }
                             }
-                            1 => {
-                                debug(&format!("read-thread: received control signal: {}", control_buf[0]));
-                                // Control signals are handled by the main event loop in pty-worker.ts
-                                // We just wake up to check for pending operations
+                            3 => { // Kill
+                                debug("read-thread: killing process");
+                                // Perform the kill operation
+                                if let Ok(mut k) = killer_clone.lock() {
+                                    let _ = k.kill();
+                                }
                             }
                             _ => {
-                                debug("read-thread: unexpected control pipe read result");
+                                debug(&format!("read-thread: unknown message type: {}", msg_type));
                             }
                         }
                     }
@@ -206,17 +232,6 @@ impl PtyImpl {
             });
         }
 
-        /* write-thread */
-        {
-            let mut wtr = master.lock().unwrap().take_writer()?;
-            thread::spawn(move || {
-                while let Ok((data, len)) = rx_w.recv() {
-                    if wtr.write_all(&data[..len]).is_err() { break; }
-                    let _ = wtr.flush();
-                }
-            });
-        }
-
         Ok(pty)
     }
 }
@@ -227,16 +242,57 @@ impl PtyTrait for PtyImpl {
     }
 
     fn write(&self, data: &[u8]) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-        self.tx_w.send((data.to_vec(), data.len())).map_err(|e| e.into())
+        // Send write command through control pipe
+        let msg_type = 1u8; // 1 = write
+        let len_bytes = (data.len() as u32).to_le_bytes();
+        
+        // Write message type
+        if unsafe { libc::write(self.control_pipe[1], &msg_type as *const u8 as *const libc::c_void, 1) } != 1 {
+            return Err("Failed to send write command".into());
+        }
+        // Write data length
+        if unsafe { libc::write(self.control_pipe[1], len_bytes.as_ptr() as *const libc::c_void, 4) } != 4 {
+            return Err("Failed to send write length".into());
+        }
+        // Write data
+        if unsafe { libc::write(self.control_pipe[1], data.as_ptr() as *const libc::c_void, data.len()) } != data.len() as isize {
+            return Err("Failed to send write data".into());
+        }
+        
+        Ok(())
     }
 
     fn resize(&self, size: PtySize) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-        self.master.lock().unwrap().resize(size).map_err(|e| e.into())
+        // Send resize command through control pipe
+        let msg_type = 2u8; // 2 = resize
+        let rows_bytes = size.rows.to_le_bytes();
+        let cols_bytes = size.cols.to_le_bytes();
+        
+        // Write message type
+        if unsafe { libc::write(self.control_pipe[1], &msg_type as *const u8 as *const libc::c_void, 1) } != 1 {
+            return Err("Failed to send resize command".into());
+        }
+        // Write rows and cols
+        if unsafe { libc::write(self.control_pipe[1], rows_bytes.as_ptr() as *const libc::c_void, 2) } != 2 {
+            return Err("Failed to send resize rows".into());
+        }
+        if unsafe { libc::write(self.control_pipe[1], cols_bytes.as_ptr() as *const libc::c_void, 2) } != 2 {
+            return Err("Failed to send resize cols".into());
+        }
+        
+        Ok(())
     }
 
     fn kill(&self) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-        let mut k = self.killer.lock().unwrap();
-        k.kill().map_err(|e| e.into())
+        // Send kill command through control pipe
+        let msg_type = 3u8; // 3 = kill
+        
+        // Write message type
+        if unsafe { libc::write(self.control_pipe[1], &msg_type as *const u8 as *const libc::c_void, 1) } != 1 {
+            return Err("Failed to send kill command".into());
+        }
+        
+        Ok(())
     }
 
     fn get_pid(&self) -> i32 {
